304a2458b96b5562cfe021d380183ccd29a974d0
Greg Griffith <cryptounitedteam@gmail.com>
2017-06-21 21:43:27 -0700

    updated the transaction page a bit

--- src/views/transactions/transactionsC.js
+++ src/views/transactions/transactionsC.js
@@ -3,6 +3,17 @@
     .module('ecc')
     .controller('transactionsC', transactionsC);
 
-  function transactionsC() {
+  function transactionsC($scope, rpc) {
+
+    console.log("done");
+
+    rpc.listtransactions("*", 100)
+      .then(txlist => console.log(txlist[0]));
+
+//    angular.forEach(obj, function(value, key) {
+//      console.log(key + ': ' + value);
+//    });
+
+
   }
 })();
