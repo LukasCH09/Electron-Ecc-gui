365745bce20e21e5bd920121a13b7b064c8a9e97
lukas <lukas.bitter@gmail.com>
2017-07-28 13:49:07 +0200

    add chinese files to get translations from POEditor + add choice in language menu

--- app/components/SettingsPage/SettingsDisplay.js
+++ app/components/SettingsPage/SettingsDisplay.js
@@ -153,6 +153,8 @@ class SettingsDisplay extends Component {
                     <div className="selectfield">
                       <select className="form-control" value={this.state.select} onChange={this.handleChange}>
                                                             <option value="bg">български (Bulgarian)</option>
+                                                            <option value="zh_hk">繁體中文-中華人民共和國香港特別行政區 (Chinese - HK)</option>
+                                                            <option value="zh_cn">简体中文—中国 (Chinese - simplified)</option>
                                                             <option value="el">ελληνικά (Greek)</option>
                                                             <option value="es">Español (Spanish)</option>
                                                             <option value="ko">한국어(Korean)</option>
@@ -165,6 +167,7 @@ class SettingsDisplay extends Component {
 					                    <option value="pt">Português (Portuguese)</option>
                                                             <option value="ru">Русский язык (Russian)</option>
                                                             <option value="vn">Tiếng việt (Vietnamese)</option>
+
                       </select>
                     </div>
                   </div>
--- app/lang/lang.js
+++ app/lang/lang.js
@@ -12,6 +12,10 @@ import lang_pl from './pl';
 import lang_tr from './tr';
 import lang_ru from './ru';
 import lang_vn from './vn';
+import lang_zh_hk from './zh_hk';
+import lang_zh_cn from './zh_cn';
+
+
 
 export function traduction() {
 		
@@ -54,6 +58,12 @@ export function traduction() {
 		}
 		else if(l == "vn"){
 			return lang_vn;
+		}
+		else if(l == "zh_hk"){
+			return lang_zh_hk;
+		}
+		else if(l == "zh_cn"){
+			return lang_zh_cn;
 		}
 		else{
 			settings.set("settings.lang","en");
